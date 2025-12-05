542ed2b262d47b909b9ad8e622198c354e0b5ead
Fauzan Mauludy <fauzan.mauludy@global.komatsu>
2025-12-05 10:33:16 +0800

    add config swaggerdoc for opeanapi.yaml file

--- src/config/apidoc.ts
+++ src/config/apidoc.ts
@@ -1,2 +1,20 @@
-import swaggerDocument from "../openapi.json";
+// Using openapi.json
+// import swaggerDocument from "../openapi.json";
+// export { swaggerDocument };
+
+// Using openapi.yaml
+import { fileURLToPath } from "url";
+import * as fs from "fs";
+import * as yaml from "js-yaml";
+import * as path from "path";
+
+// declare js syntax from ts
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+// path ke file YAML
+const yamlPath = path.resolve(__dirname, "../openapi.yaml");
+
+const fileContents = fs.readFileSync(yamlPath, "utf8");
+let swaggerDocument: any = yaml.load(fileContents);
+
 export { swaggerDocument };
