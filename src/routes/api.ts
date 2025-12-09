cdb5aa70dfcda84b5e22c792fc54fef8797e81f5
Fauzan Mauludy <fauzan.mauludy@global.komatsu>
2025-12-09 09:03:51 +0800

    add shift fuel time loss api

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import currentFuelTimeLossController from "../controller/current-fueltimeloss-controller";
 import shiftSummaryFueltimelossController from "../controller/shift-summary-fueltimeloss-controller";
+import shiftFueltimelossController from "../controller/shift-fueltimeloss-controller";
 
 const userRouter = express.Router();
 // userRouter.use(authMiddleware)
 
 userRouter.get("/v2/fuelTimeLoss/current", currentFuelTimeLossController.get);
+userRouter.get("/v2/fuelTimeLoss/shift", shiftFueltimelossController.get);
 userRouter.get(
   "/v2/fuelTimeLoss/shift/summary",
   shiftSummaryFueltimelossController.get
---
+++ src/service/shift-fueltimeloss-service.ts
@@ -0,0 +1,23 @@
+import { prismaMainMinecare } from "../lib/main-minecare-prisma-client";
+import { ResponseError } from "../error/error-response";
+import { logger } from "../application/logging";
+// import { dataConverter } from "./fueltimeloss-format-service";
+
+const get = async () => {
+  try {
+    const data = await prismaMainMinecare.pitFuelTimeLossShift.findMany({});
+
+    // const result = await dataConverter(data);
+    return data;
+  } catch (error: any) {
+    logger.error("Database or Unhandled Error in Service:", error);
+    throw new ResponseError(
+      500,
+      `Database connection failed : ${error.message}`
+    );
+  }
+};
+
+export default {
+  get,
+};
